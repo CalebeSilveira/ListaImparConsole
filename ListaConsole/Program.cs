c53ce447d02e8bf928b551df1c1857da2ddbf1fe
CalebeSilveira <calebe_mendes.silveira@hotmail.com>
2021-08-19 17:39:21 -0300

    Lista de numeros Impares, Questão Aval Group

---
+++ ListaConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Int32> NumerosPares;
+            NumerosPares = new List<Int32>();
+
+            for (int i = 0; i < 11; i++)
+            {
+                    NumerosPares.Add(i);
+            }
+            foreach (var item in NumerosPares)
+            {
+                if (item % 2 == 0)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
